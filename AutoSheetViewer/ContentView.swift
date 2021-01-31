55299cfc427318b964aeecd4a8ab754de2b88421
shahanneda <shahan.neda@gmail.com>
2021-01-30 16:42:45 -0800

    Added Beethoven pitch detection library

--- AutoSheetViewer/ContentView.swift
+++ AutoSheetViewer/ContentView.swift
@@ -7,12 +7,14 @@
 //
 
 import SwiftUI
+import Beethoven
+import Pitchy
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
         HStack{
-            Button(action: {
-            }) {
+            Button(action: SetupPitch) {
                 Text("Detect Pitch")
             }
         }
@@ -20,6 +22,30 @@ struct ContentView: View {
     
     func SetupPitch(){
         print("Setting Up Pitch")
+        // Creates a configuration for the input signal tracking (by default).
+        let config = Config(
+          bufferSize: 4096,
+          estimationStrategy: .yin
+        )
+
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case AVAudioSessionRecordPermission.granted:
+            print("Permission granted")
+        case AVAudioSessionRecordPermission.denied:
+            print("Pemission denied")
+        case AVAudioSessionRecordPermission.undetermined:
+            print("Request permission here")
+            AVAudioSession.sharedInstance().requestRecordPermission({ (granted) in
+                // Handle granted
+            })
+        }
+
+        let delagate = PitchEngineDelegateImplementor();
+        let pitchEngine = PitchEngine(config: config, delegate: delagate)
+        pitchEngine.start()
+
+
+
     }
 }
 
@@ -28,4 +54,18 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+class PitchEngineDelegateImplementor: PitchEngineDelegate {
+  func pitchEngine(_ pitchEngine: PitchEngine, didReceivePitch pitch: Pitch) {
+        print(pitch.note.string)
+  }
+
+  func pitchEngine(_ pitchEngine: PitchEngine, didReceiveError error: Error) {
+    print(error)
+  }
+
+  public func pitchEngineWentBelowLevelThreshold(_ pitchEngine: PitchEngine) {
+    print("Below level threshold")
+  }
 }
